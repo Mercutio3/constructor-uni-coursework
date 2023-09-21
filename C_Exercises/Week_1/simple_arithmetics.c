172898746ca6b1345650e17b767b6db57b175847
Santiago <68315774+Mercutio3@users.noreply.github.com>
2023-09-21 17:21:10 +0200

    Code description & comments

--- C_Exercises/Week_1/simple_arithmetics.c
+++ C_Exercises/Week_1/simple_arithmetics.c
@@ -1,25 +1,27 @@
 /*
-CH-230-A
-a1_p4.c 
-Santiago Dominguez
-sdominguez@jacobs-university.de
+This program computes some basic arithmetic operations and prints the results.
+The goal is to demonstrate usage of five basic operators.
 */
 
 #include <stdio.h>
 
 int main() {
+    //Declare variables
     int x = 17, y = 4;
     float z = y;
-    
+
+    //Print variables
     printf("x=%d\n", x);
     printf("y=%d\n", y);
-    
+
+    //Perform caltulations
     int sum = x + y;
     int product = x * y;
     int difference = x - y;
     float quotient = x / z;
     int remainder = x % y;
-    
+
+    //Print results
     printf("sum=%d\n", sum);
     printf("product=%d\n", product);
     printf("difference=%d\n", difference);
